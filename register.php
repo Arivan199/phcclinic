36e45e5f3891d3ca04453b1e6cbb903286c25c5a
Arivan T <63254290+Arivan199@users.noreply.github.com>
2022-01-11 19:31:42 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
     $servername = 'us-cdbr-east-05.cleardb.net';
     $username = 'b0853c579d7b5d';
     $password = '3551bcb1';
-    $db = 'patient';
+    $db = 'heroku_04551752c2bc552';
     // Create connection
     $conn =new mysqli($servername,$username,$password,$db);
     if(!$conn)
