0df1a26477228183c6343eb8243f321d8460ca33
Arivan T <63254290+Arivan199@users.noreply.github.com>
2022-01-11 19:24:04 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    $db = 'patient';
+    // Create connection
+    $conn =new mysqli($servername,$username,$password,$db);
+    if(!$conn)
+    echo 'Connection failed';
+    else
+    echo 'connected';
+    //phpinfo();
+    echo 'shit';
+    if(isset($_POST['submit'])){
+        echo 'shit';
+    $name=$_POST['name'];
+    $Aadhar_no=$_POST['Aadhar'];
+    $phone=$_POST['mobile'];
+    $dob=$_POST['dob'];
+    $addr1=$_POST['add1'];
+    $addr2=$_POST['add2'];
+    $addr3=$_POST['add3'];
+    $addd=$addr1.$addr2;
+    $addr=$addd.$addr3;
+    $his=$_POST['his'];
+    $dp=$_POST['image'];
+    echo 'shit';
+    $query ="insert into patient(Aadhar_number,patient_id,patient_name,address,dp,mobile,dob,medical_history) values('$Aadhar_no','$Aadhar_no','$name','$addr','$dp','$phone','$dob','$his')";
+    
+    $run=mysqli_query($conn,$query);
+    echo 'shit';
+    if($run){
+        echo 'Succesfully inserted';
+    }
+    else{
+        echo 'Insertion failed';
+    }
+    }
+    echo 'shit';
+?>
